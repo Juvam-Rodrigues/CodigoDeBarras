e08589639a7e43ddd81614fe99ad26048c4f158d
Juvam Rodrigues <juvamrodrigues17@gmail.com>
2025-01-16 10:59:36 -0300

    Verificação de formato, e de possível existência do arquivo

--- src/leitorCodigo.c
+++ src/leitorCodigo.c
@@ -6,18 +6,48 @@
 #include "../include/codigo.h"
 #include "../include/arquivo.h"
 
-int main(int argc, char **argv){
-  Arquivo leitor;
-  for(int i = 1; i < argc; i++){
-      switch (i)
-      {
-      case 1:
-          strcpy(leitor.nome, argv[1]);
-        break;
-      default:
-        break;
-      }
-  }
-  printf("%s", leitor.nome);
+int main(int argc, char **argv)
+{
+    Arquivo leitor;
+    for (int i = 1; i < argc; i++)
+    {
+        switch (i)
+        {
+        case 1:
+            strcpy(leitor.nome, argv[1]);
+            break;
+        default:
+            break;
+        }
+    }
 
+    FILE *arquivo = fopen(strcat(leitor.nome, ".pbm"), "r");
+    if (arquivo == NULL)
+    {
+        printf("Arquivo não existe.");
+        return 1;
+    }
+
+    char formato[3];
+    fscanf(arquivo, "%2s", formato);
+
+    if (formato[0] != 'P' || formato[1] != '1')
+    {
+        printf("O arquivo não está no formato PBM.");
+        return 1;
+    }
+
+    int largura, altura;
+    char **imagem;
+
+    fscanf(arquivo, "%d %d", largura, altura);
+    *imagem = (int *)malloc((largura) * (altura) * sizeof(int));
+    if (!*imagem)
+    {
+        perror("Erro ao alocar memória para a imagem");
+        fclose(arquivo);
+        exit(1);
+    }
+
+    printf("%s", leitor.nome);
 }
